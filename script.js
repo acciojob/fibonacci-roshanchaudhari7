3e533b06fae7e75b887e1085d1f7a75e800c83d4
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-07-12 19:51:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function fibonacci(num) {
 // your code here
+	if(num < 0) {
+	    return "Invalid Input";
+	}
+	if(num === 0) {
+		return 0;
+	}
+	if(num === 1) {
+	    return 0;
+	}
+	if(num === 2) {
+	    return 1;
+	}
+	let x = 0;
+	let y = 1;
+	for(let i = 3; i <= num; i++) {
+	    let temp = x + y;
+	    x = y;
+	    y = temp;
+	}
+	return y;
 }
 
 module.exports = fibonacci;
